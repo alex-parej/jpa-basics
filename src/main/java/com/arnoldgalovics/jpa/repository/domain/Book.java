a4b6581f25177886029a054aa40a341210060c42
Alex Parej <Alex_Parej@epam.com>
2017-09-18 17:07:34 +0200

    init Book model

---
+++ src/main/java/com/arnoldgalovics/jpa/repository/domain/Book.java
@@ -0,0 +1,108 @@
+package com.arnoldgalovics.jpa.repository.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @Basic(optional = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Basic(optional = false)
+    private String name;
+    @Basic(optional = false)
+    private float price;
+    @Basic(optional = false)
+    private int pages;
+    @Basic(optional = false)
+    private String isbn;
+    @Basic(optional = false)
+    @Enumerated(EnumType.STRING)
+    private Genre genre;
+
+    public Book(int id, String name, float price, int pages, String isbn, Genre genre) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.pages = pages;
+        this.isbn = isbn;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", pages=" + pages +
+                ", isbn='" + isbn + '\'' +
+                ", genre=" + genre +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
+
---
+++ src/main/java/com/arnoldgalovics/jpa/repository/domain/Genre.java
@@ -0,0 +1,7 @@
+package com.arnoldgalovics.jpa.repository.domain;
+
+public enum Genre {
+    COMICS,
+    HORROR,
+    FANTASY
+}
